743b5fd19bc5267de414fa1a38b07766d2d2739c
Denis Surin <pqdubpq@gmail.com>
2016-05-03 23:53:56 +0200

    Add and amend echos

--- dz1-1.php
+++ dz1-1.php
@@ -1,20 +1,26 @@
+<?php
 /*
     Создайте 5 переменных с разными скалярными типами и присвойте им
     произвольные значения. Затем создайте переменные: $name, $age, $city, $job и
     присвойте им соответствующие данные. $job – логическая (да/нет). Есть работа? С
     помощью echo выведите все переменные в удобочитаемом виде.
 */
-<?php
-    $bool = false;      // Логический тип
+
+    $bool = true;      // Логический тип
     $int = 5;           // Integer - целочисленный тип
-    $float = 2.6;       // Float - число с плавающей точкой
+    $float = 2.6;       // Float - число с плавающей точкой (Double)
     $string = 'Строка'; // String - Строчный тип
 
     # 5-й скалярный тип данных PHP не знаю
+
+    echo "$bool - ".gettype($bool).'<br>';
+    echo "$int - ".gettype($int).'<br>';
+    echo "$float - ".gettype($float).'<br>';
+    echo "$string - ".gettype($string).'<br>'.'<hr>';
 
     $name = "Denis";
     $age = 25;
     $city = 'Nuremberg';
     $job = false;
 
-    echo "";
+    echo "Hi! My name is <b>$name</b> and I'm $age. I live in $city ";
