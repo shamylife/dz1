5acfa6903e4bb917c81ada11bd4bdb196d41f9cf
Denis Surin <pqdubpq@gmail.com>
2016-05-09 01:34:18 +0200

    First Part is Complete

--- dz1-9.php
+++ dz1-9.php
@@ -1,3 +1,4 @@
+<?php
 /*
     Создайте переменную $str, которой присвойте строковое значение,
     содержащее отдельные слова разделённые пробелом. Выведите строку на экран.
@@ -5,3 +6,25 @@
     или do-while (на ваше усмотрение) развернуть массив и склеить в строку используя
     любой символ, кроме пробела. Вывести результат.
 */
+
+    $str = "Арбуз Абрикос Слива Яблоко Груша";
+
+    echo $str . '<br>';
+
+    $arr = explode(' ', $str);
+
+    echo '<pre>' . print_r($arr, true) . '</pre>';
+
+    $length = count($arr);
+    $reverse_array = Array();
+
+    while ($length) {
+        $reverse_array[] = $arr[$length - 1];
+        $length--;
+    }
+
+    echo '<pre>' . print_r($reverse_array, true) . '</pre>';
+
+//    $newstr = implode(' & ', $reverse_array);
+//
+//    echo $newstr;
