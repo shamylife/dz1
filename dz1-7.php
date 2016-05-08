6d360c8b358929bafb946f1e944144e7b62a516f
Denis Surin <pqdubpq@gmail.com>
2016-05-08 23:59:37 +0200

    Amended Array

--- dz1-7.php
+++ dz1-7.php
@@ -5,21 +5,14 @@
     Вывести массив в удобочитаемом виде через конструкцию print_r и через foreach.
 */
 
-    $cars = Array (
-        'model' => "X5",
-        'speed' => "120",
-        'doors' => "5",
-        'year'  => "2015",
-        'deep' => Array (
-            'model' => "Land Cruiser",
-            'speed' => "100",
-            'doors' => "5",
-            'year'  => "2014",
-            'deep'  => Array (
-                'model' => "Frontera",
-                'speed' => "80",
-                'doors' => "5",
-                'year'  => "2003",
-            )
-        )
-    );
+    $cars = [
+        'BMW'    => ['model' => 'X5', 'speed' => 120, 'doors' => 5, 'year' => 2015],
+        'Toyota' => ['model' => 'Land Cruiser', 'speed' => 100, 'doors' => 5, 'year' => 2014],
+        'Opel'   => ['model' => 'Frontera', 'speed' => 80,  'doors' => 5, 'year' => 2003]
+    ];
+
+    echo '<pre>'.print_r($cars, true).'</pre><br><hr>';
+
+    foreach ($cars as $key => $value) {
+        echo $key.': '.$value.'<br>';
+    }
